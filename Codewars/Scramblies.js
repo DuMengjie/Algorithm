0a9aea9175db557efea61047a7119c31460d0745
DuMengjie <dumengjie2016@gmail.com>
2017-04-26 22:10:47 +0800

    20170426

---
+++ Codewars/Scramblies.js
@@ -0,0 +1,33 @@
+// Instructions:
+// Write function scramble(str1,str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
+//
+// For example:
+// str1 is 'rkqodlw' and str2 is 'world' the output should return true.
+// str1 is 'cedewaraaossoqqyt' and str2 is 'codewars' should return true.
+// str1 is 'katas' and str2 is 'steak' should return false.
+//
+// Sample Tests:
+// describe('Intial Tests', function(){
+//   Test.assertEquals(scramble('rkqodlw','world'),true);
+//   Test.assertEquals(scramble('cedewaraaossoqqyt','codewars'),true);
+//   Test.assertEquals(scramble('katas','steak'),false);
+//   Test.assertEquals(scramble('scriptjava','javascript'),true);
+//   Test.assertEquals(scramble('scriptingjava','javascript'),true);
+//   Test.assertEquals(scramble('scriptsjava','javascripts'),true);
+//   Test.assertEquals(scramble('jscripts','javascript'),false);
+//   Test.assertEquals(scramble('aabbcamaomsccdd','commas'),true);
+// });
+//
+// Best Practice
+// function inArray(arr1, arr2) {
+//   return arr1.filter(function(needle) {
+//     return arr2.some(function(haystack) {
+//       return haystack.indexOf(needle) > -1;
+//     });
+//   }).sort();
+// }
+
+function scramble(str1, str2) {
+ //code me
+ 
+}
