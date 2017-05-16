4f70ddad50824bbffae43a86eb096bc352ddd27e
DuMengjie <dumengjie2016@gmail.com>
2017-05-16 20:24:12 +0800

    2017016

---
+++ Codewars/Tank Truck.js
@@ -0,0 +1,33 @@
+// Instructions:
+// To introduce the problem think to my neighbor who drives a tanker truck. The level indicator is down and he is worried because he does not know if he will be able to make deliveries. We put the truck on a horizontal ground and measured the height of the liquid in the tank.
+
+// Fortunately the tank is a perfect cylinder and the vertical walls on each end are flat. The height of the remaining liquid is h, the diameter of the cylinder is d, the total volume is vt (h, d, vt are positive or null integers). You can assume that h <= d.
+
+// Could you calculate the remaining volume of the liquid? Your function tankvol(h, d, vt) returns an integer which is the truncated result (e.g floor) of your float calculation.
+
+// Examples:
+
+// tankvol(40,120,3500) should return 1021 (calculation gives about: 1021.26992027)
+
+// tankvol(60,120,3500) should return 1750
+
+// tankvol(80,120,3500) should return 2478 (calculation gives about: 2478.73007973)
+//
+// Test Examples
+//
+// Test.describe("Tank Volume",function() {
+// Test.it("Basic Tests",function() {
+// Test.assertEquals(tankvol(5, 7, 3848), 2940)
+// Test.assertEquals(tankvol(2, 7, 3848), 907)
+// })})
+//
+// Best Practices
+// function balance(left,right){
+//    l=[...left].reduce((s,a)=>s+(a=='?'?3:2),0);
+//    r=[...right].reduce((s,a)=>s+(a=='?'?3:2),0);
+//    return l==r?"Balance":l>r?"Left":"Right"
+// }
+
+function tankvol(h, d, vt) {
+    // your code
+}
